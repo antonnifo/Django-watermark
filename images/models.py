62a4f800ea2223f3b90a0b15203462882f562b17
antonnifo <antonnifo@live.com>
2023-03-22 16:06:55 +0300

    add watermark model

--- images/models.py
+++ images/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from watermarker.models import Watermark
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Watermark(Watermark):
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='watermarks')
+
