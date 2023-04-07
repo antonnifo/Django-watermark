263b1ac800a695667abd2db6f2ad318d67574135
antonnifo <antonnifo@live.com>
2023-04-07 09:28:24 +0300

    add view to upload images

--- images/views.py
+++ images/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import WatermarkForm
+from .forms import WatermarkForm, ImageAddForm
 from django.contrib.auth.decorators import login_required
+from .models import Image
 
 
 @login_required
@@ -17,3 +18,18 @@ def add_watermark(request):
 
     return render (request, 'images/add_watermark.html' ,{'form':form,})
 
+@login_required
+def watermark_images(request):
+    if request.method == 'POST':
+        form = ImageAddForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.added_by = request.user
+            image.save()
+            return redirect('dashboard')
+    else:
+        images = Image.objects.all()
+        form = ImageAddForm()
+
+    return render(request, 'images/watermark_image.html', {'form': form,
+                                                            'images':images, })
