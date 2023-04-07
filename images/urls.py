10d06691041a22f4edfed2c605e6e5fc3a15e770
antonnifo <antonnifo@live.com>
2023-04-07 09:30:26 +0300

    add pattern for adding images view

--- images/urls.py
+++ images/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'images'
 urlpatterns = [
-                 path('add/', views.add_watermark, name='add_watermark'),
+    path('add/', views.add_watermark, name='add_watermark'),
+    path('upload/', views.watermark_images, name='add_image'),
 ]
