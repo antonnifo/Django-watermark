edfdafdf5393e9651d3e8cf59e11ac8c2d7bfaca
antonnifo <antonnifo@live.com>
2023-04-07 09:25:20 +0300

    add image add form

--- images/forms.py
+++ images/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import Watermark
+from .models import Watermark, Image
 
 class WatermarkForm(forms.ModelForm):
-    
     class Meta:
         model = Watermark
+        fields = ("name", "image",)
+
+class ImageAddForm(forms.ModelForm):
+    class Meta:
+        model = Image
         fields = ("name", "image",)
