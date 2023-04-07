cce6dd9874c3367d39c8bb515762c5cae3f22c32
antonnifo <antonnifo@live.com>
2023-04-07 09:26:29 +0300

    add image model

--- images/models.py
+++ images/models.py
@@ -4,5 +4,11 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Watermark(Watermark):
-    added_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='watermarks')
+    added_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='watermarks')
 
+class Image(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="images/")
+    added_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='images')
