84352ac5d4d596e41c8aaf85a8dd0ffce02d639c
antonnifo <antonnifo@live.com>
2023-04-09 22:22:45 +0300

    rename watermark url

--- images/urls.py
+++ images/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'images'
 urlpatterns = [
     path('add/', views.add_watermark, name='add_watermark'),
-    path('upload/', views.watermark_images, name='add_image'),
+    path('upload/', views.watermark_image, name='add_image'),
     path('images/', views.list_images, name='list_images'),
 ]
